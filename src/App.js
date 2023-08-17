e15001e3293c24f612e19f8e646a7f21d5f389f1
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2023-08-17 21:05:30 +0300

    lesson 18

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './../src/components/Header/Header';
 import Navbar from './../src/components/Navbar/Navbar';
 import Profile from './../src/components/Profile/Profile';
+import Dialogs from './../src/components/Dialogs/Dialogs';
 
 const App = () => {
   return (
@@ -9,7 +10,10 @@ const App = () => {
       <div className="app-wrapper">
         <Header />
         <Navbar />
-        <Profile />
+        <div className="app-wrapper-content">
+          {/* <Profile /> */}
+          <Dialogs />
+        </div>
       </div>
     </div>
   );
