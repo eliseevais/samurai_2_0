b8cb04f4443b59f5f94300f1dd8d457d83b44c22
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2023-08-28 22:47:36 +0300

    lesson 31

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import styles from "./Dialogs.module.css"
 import { DialogsItem } from "./DialogsItem/DialogsItem";
 import { Message } from "./Message/Message";
+import React from "react";
 
 
 const Dialogs = (props) => {
@@ -12,13 +13,26 @@ const Dialogs = (props) => {
   let messagesArray = props.messages.map(message =>
     <Message message={message.message} key={message.id} />)
 
+  let sendMessageButton = () => {
+    let text = newMessageElement.current.value;
+    alert(text)
+  }
+
+  let newMessageElement = React.createRef();
+
   return (
     <div className={styles.dialogs}>
       <div className={styles.dialogItem}>
         {dialogsArray}
       </div>
-      <div className={styles.messages}>
+      <div className={styles.dialogWrapper}>
+        <div className={styles.messages}>
         {messagesArray}
+      </div>
+      <div className={styles.addNewMessage}>
+      <textarea ref={newMessageElement}></textarea>
+      <button onClick={sendMessageButton}>Send</button>
+      </div>
       </div>
     </div>
   )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,25 @@
 import styles from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import React from "react";
 
 const MyPosts = (props) => {
 
   let postMessages = props.posts.map(post => <Post message={post.postText}
     likesCount={post.likesCount} key={post.id} />)
 
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text)
+  }
+
+  let newPostElement = React.createRef();
+
   return (
     <div className={styles.posts}>
       <h4>My posts</h4>
       <div className={styles.addPostField}>
-        <textarea></textarea>
-        <button>Add post</button>
+        <textarea ref={newPostElement}></textarea>
+        <button onClick={addPost}>Add post</button>
       </div>
       {postMessages}
     </div>
